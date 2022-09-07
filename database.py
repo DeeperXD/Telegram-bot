a9fae812526264305ec23f17ed1238b1769b905b
DeeperXD <97693451+DeeperXD@users.noreply.github.com>
2022-09-07 15:35:50 +0300

    Basic database managment

--- database.py
+++ database.py
@@ -3,16 +3,63 @@ import sqlite3
 
 # контакт дс, про себе, підписка на новини та івенти, стать
 def add_user(user):
-    pass
+    sql_command(f"INSERT INTO users VALUES ({user.telegram_id})")
 
 
-def remove_user(user_id):
-    pass
+def remove_user(telegram_id):
+    sql_command(f"DELETE FROM users WHERE telegram_id = {telegram_id}")
 
 
 def check_user_exist(user_id):
-    pass
+    res = sql_command_r(f'SELECT * FROM users WHERE telegram_id == {user_id}')
+    return len(res) > 0
 
 
+def sql_command(command):
+    conn = sqlite3.connect('users.dt')
+    c = conn.cursor()
+    c.execute(command)
+
+    conn.commit()
+    conn.close()
+
+
+def sql_command_r(command):
+    conn = sqlite3.connect('users.dt')
+    c = conn.cursor()
+    c.execute(command)
+    res = c.fetchall()
+    conn.commit()
+    conn.close()
+
+    return res
+
+
+async def sql_command_promp():
+    command = input("enter sql command")
+    conn = sqlite3.connect('users.dt')
+    c = conn.cursor()
+    try:
+        c.execute(command)
+    except:
+        print(f"error to execute command")
+
+    conn.commit()
+    conn.close()
+
+
+async def sql_print():  # SELECT * FROM users
+    command = input("enter sql command: ")
+    conn = sqlite3.connect('users.dt')
+    c = conn.cursor()
+    try:
+        c.execute(command)
+        print(c.fetchall())
+    except:
+        print(f"error to execute command")
+
+
+    conn.commit()
+    conn.close()
 
 
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import telebot
 from telebot import  types
 
+import asyncio
+
 import json
 import atexit
 import database
@@ -12,15 +14,31 @@ bot = telebot.TeleBot(TOKEN)
 ADMINS_ID = []
 GROUP_ID = cred.GROUP_TOKEN
 
+class User:
+    def __init__(self, telegram_id):
+        self.telegram_id = telegram_id
+
 
 @bot.message_handler(commands=['start', 'info']) # початок діалогу з ботом
 def send_start_message(message):
     markup = types.InlineKeyboardMarkup()
     markup.row_width = 1
+
+    print(message.from_user.id)
+    if not database.check_user_exist(message.from_user.id):
+        markup.add(types.InlineKeyboardButton("Зареєструватися", callback_data="register"))
+    else:
+        markup.add(types.InlineKeyboardButton("Додати форму", callback_data="add form"),
+                   types.InlineKeyboardButton("Видалити інфо про себе", callback_data="remove info")
+                   )
+
+
+
     markup.add(
-        types.InlineKeyboardButton("Зареєструватися", callback_data="register"),
         types.InlineKeyboardButton("Інфо про реєстрацію", callback_data='register info'),
-        types.InlineKeyboardButton("Діскорд сервер", url="https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+        types.InlineKeyboardButton("Діскорд сервер", url="https://www.youtube.com/watch?v=dQw4w9WgXcQ"),
+        types.InlineKeyboardButton("Телеграм группа", url="https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+
     )
 
     bot.send_message(message.chat.id, 'Привіт, я бот ФІКТ, і я допоможу тобі бути вкурсі новин'
@@ -30,17 +48,30 @@ def send_start_message(message):
 
 
 @bot.callback_query_handler(lambda query: query.data == 'register')
-def register(query): # реєстрація
-    bot.send_message(query.message.chat.id, 'Congrats you started register')
+def register(query):
+    if database.check_user_exist(query.from_user.id):
+        bot.send_message(query.message.chat.id, 'You are registered, now, you cant register one more time')
+        return
+
+    database.add_user(User(query.from_user.id))
+    bot.send_message(query.message.chat.id, 'Congrats you register')
 
     # потрібно описати етапи реєстрації
+
+
+@bot.callback_query_handler(lambda query: query.data == 'remove info')
+def remove_info(query):
+    print(query.from_user.id)
+
+    if database.check_user_exist(query.from_user.id):
+        database.remove_user(query.from_user.id)
+        bot.send_message(query.message.chat.id, 'Вся інформація про вас видалена.')
 
 
 @bot.callback_query_handler(lambda query: query.data == 'register info')
 def register(query):  # реєстрація
     bot.send_message(query.message.chat.id, 'Після реєстрації можна добавити додаткову інформцію про себе')
 
-    # потрібно описати етапи реєстрації
 
 @bot.message_handler(commands=['getform'])
 def send_form(message):
@@ -65,10 +96,6 @@ def on_message(message):
         return
 
 
-def send_data(text):
-    bot.send_message(GROUP_ID, text)
-
-
 def write_data():
     pass
 
@@ -80,23 +107,34 @@ def at_exit():
 atexit.register(at_exit)
 
 print("bot started")
+
+
+# region CONSOLE
+
+
+commands = {
+    'db': database.sql_command_promp,
+    'p db': database.sql_print,
+}
+
+
+async def console_input():
+    text = input('Enter command: ')
+    if text in commands:
+        await commands[text]()
+    else:
+        print('This command dont exist')
+    await console_input()
+
+# endregion
+
 bot.polling()
-
-
 
 # Привіт, я бот ФІКТ, і я допоможу тобі бути вкурсі новин, та познайомитися з новими друзями
 # за допомогою команд ти зможеш зареєструвати себе в системі знаймств,
 # не переживай ці дані ми ніяк не застосовуємо, це просто твоя анкета)
 # напиши команду /set description і після цього відправ короткий опис про себе)
 # напиши команду /set form і після цього відправ заповнену форму
-
-# лінк на наш сервер дс
-# лінк на телеграм группу
-
-
-
-
-
 
 
 # TODO: реєстрація юзера в системі, а саме, його опис, стать і заповнена форма (це буде по бажанню)
